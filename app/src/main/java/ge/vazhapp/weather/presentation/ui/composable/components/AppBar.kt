6956ca9665c740ad70dcbbc7183601cfd2aabd8d
Vazhapp <vkentchiashvili@gmail.com>
2023-08-17 20:30:39 +0400

    Added font and implemented Top App Bar, also ripple color for the whole application.

--- app/src/main/java/ge/vazhapp/weather/presentation/ui/composable/components/AppBar.kt
+++ app/src/main/java/ge/vazhapp/weather/presentation/ui/composable/components/AppBar.kt
@@ -1,32 +1,94 @@
 package ge.vazhapp.weather.presentation.ui.composable.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ge.vazhapp.weather.R
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun HomeScreenTopBar(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onLocationChangeClick: () -> Unit,
+    onSettingsClick: () -> Unit,
 ) {
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(12.dp)
+            .fillMaxHeight()
+            .padding(all = 12.dp)
     ) {
         Row {
-            Text(
-                text = "Tbilisi",
-                color = Color.White,
-                fontSize = 24.sp
+            Column {
+                Spacer(modifier = modifier.height(36.dp))
+
+                Row(
+                    modifier = modifier
+                        .clickable {
+                            onLocationChangeClick()
+                        }
+                ) {
+                    Text(
+                        text = "Tbilisi",
+                        color = Color.White,
+                        fontSize = 38.sp,
+                        fontFamily = FontFamily(fonts = listOf(Font(R.font.main_font)))
+                    )
+
+                    Image(
+                        modifier = modifier
+                            .padding(start = 8.dp)
+                            .align(Alignment.CenterVertically),
+                        painter = painterResource(id = R.drawable.ic_location),
+                        contentDescription = "",
+                    )
+
+                    Image(
+                        modifier = modifier
+                            .align(Alignment.CenterVertically)
+                            .size(28.dp),
+                        painter = painterResource(id = R.drawable.ic_arrow_drop_down),
+                        contentDescription = ""
+                    )
+                }
+            }
+            Spacer(
+                modifier = modifier
+                    .height(116.dp)
+                    .weight(1f)
+            )
+
+            Image(
+                modifier = modifier
+                    .size(32.dp)
+                    .align(Alignment.CenterVertically)
+                    .clickable {
+                        onSettingsClick()
+                    },
+                painter = painterResource(id = R.drawable.ic_settings),
+                contentDescription = ""
             )
         }
     }
---
+++ app/src/main/java/ge/vazhapp/weather/presentation/ui/theme/WeatherRippleTheme.kt
@@ -0,0 +1,23 @@
+package ge.vazhapp.weather.presentation.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+object WeatherRippleTheme : RippleTheme {
+    // Here you should return the ripple color you want
+    // and not use the defaultRippleColor extension on RippleTheme.
+    // Using that will override the ripple color set in DarkMode
+    // or when you set light parameter to false
+    @Composable
+    override fun defaultColor(): Color = LightBlue
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
+        Color.Black,
+        lightTheme = !isSystemInDarkTheme()
+    )
+}
