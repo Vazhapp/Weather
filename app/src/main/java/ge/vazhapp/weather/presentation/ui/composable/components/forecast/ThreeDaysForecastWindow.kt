6c778cce92a1eee8530f230d137ffff03b8f54ab
Vazhapp <vkentchiashvili@gmail.com>
2023-08-29 19:47:18 +0400

    added little slide animation on forecast window

--- app/src/main/java/ge/vazhapp/weather/presentation/ui/composable/components/forecast/ThreeDaysForecastWindow.kt
+++ app/src/main/java/ge/vazhapp/weather/presentation/ui/composable/components/forecast/ThreeDaysForecastWindow.kt
@@ -1,5 +1,10 @@
 package ge.vazhapp.weather.presentation.ui.composable.components.forecast
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -27,19 +33,37 @@ fun ThreeDaysForecastWindow(
         pathEffect = PathEffect.dashPathEffect(floatArrayOf(30f, 20f), 0f)
     )
 
-    Box(
-        modifier = modifier
-            .height(200.dp)
-            .fillMaxWidth()
-            .drawBehind {
-                drawRoundRect(color = Color.White, style = stroke)
-            }
-            .background(Color.White.copy(alpha = 0.25f)),
-        contentAlignment = Alignment.BottomCenter,
+    val visibleState = remember { MutableTransitionState(false) }
+    visibleState.targetState = true
+
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = slideInVertically(
+            initialOffsetY = { fullHeight -> fullHeight },
+            animationSpec = tween(
+                durationMillis = 400,
+                easing = LinearOutSlowInEasing
+            )
+        ),
     ) {
-        LazyRow(modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
-            items(3) {
-                EachDayCard(weatherTypeImageUrl = "https://cdn.weatherapi.com/weather/64x64/day/116.png")
+        Box(
+            modifier = modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .drawBehind {
+                    drawRoundRect(color = Color.White, style = stroke)
+                }
+                .background(Color.White.copy(alpha = 0.25f)),
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            LazyRow(
+                modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                items(3) {
+                    EachDayCard(weatherTypeImageUrl = "https://cdn.weatherapi.com/weather/64x64/day/116.png")
+                }
             }
         }
     }
